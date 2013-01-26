215e22facbf3a02118713da0952fc00875ac067e
Bruce Williams <bruwillimail@gmail.com>
2013-01-25 17:14:14 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tmorg::Application.config.secret_token = '70d30fb75d7cd641da9c9a9647a22c45e33c1ecdc926a93f512f0a25c7511fd4d5504842ee81d4334a374f291718a36af5babb58b9a5388a2cdd2fb0b125a3b4'
---
+++ spec/support/utilities.rb
@@ -0,0 +1,40 @@
+include ApplicationHelper
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_title do |text|
+  match do |page|
+    page.should have_selector('title', text: text)
+  end
+end
+
+RSpec::Matchers.define :have_any_error_message do
+  match do |page|
+    page.should have_selector('div.alert.alert-error')
+  end
+end
+
+RSpec::Matchers.define :have_heading do |text|
+  match do |page|
+    page.should have_selector('h1', text: text)
+  end
+end
+
+def user(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
